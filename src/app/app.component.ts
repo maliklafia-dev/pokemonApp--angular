b5f21310382ec56e4a7eb5481e256fa6c183c1b7
malik LAFIA <maliksero.lafia@gmail.com>
2024-04-18 15:13:58 +0200

    initialisation du projet pokemon

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+      <h1>
+        Welcome to {{title}}! </h1>`,
+})
+
+export class AppComponent {
+  title = 'ng-pokemon-app';
+}
