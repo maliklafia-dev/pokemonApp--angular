7cc88b7094a7f33d95acd7fdc0493b7546372183
malik LAFIA <maliksero.lafia@gmail.com>
2024-04-20 20:31:13 +0200

    import de la liste des pokemon et vérifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   template: `
       <h1>
-        Welcome to {{title}}! </h1>`,
+        Liste de Pokémons</h1>`,
 })
 
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+      console.table(this.pokemonList);
+      this.selectPokemon(this.pokemonList[1]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
+
 }
