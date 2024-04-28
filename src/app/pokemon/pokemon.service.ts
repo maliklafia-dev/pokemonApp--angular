1ad1390334585997685f2eae84af0fd0205fb453
malik LAFIA <maliksero.lafia@gmail.com>
2024-04-28 04:01:58 +0200

    Ajout d'une web-api virtuelle dans angular, configuration des requettes get pour afficher les diiférents pokemons dans les composants list et détails pokemon avec abonnement au observables

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemon-list';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+
+  constructor(private http:HttpClient) {}
+
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((pokemonList) => this.log(pokemonList)),
+      catchError((error) => this.handleError(error, []))
+    )
   }
 
-  getPokemonById(pokemonId: number): Pokemon|undefined {
-    return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap((pokemon) => this.log(pokemon)),
+      catchError((error) => this.handleError(error, undefined))
+    );
   }
+
+
+  private log(response: Pokemon[] | Pokemon | undefined){
+    console.table(response);
+ }
+
+ private handleError(error: Error, errorValue: any) {
+  console.error(error);
+  return of(errorValue);
+ }
+
 
   getPokemonTypeList(): string[] {
     return [
